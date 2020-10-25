3eb0dac51ae413145293ff6ea34a0c68747bbeea
Harshad <harshad.satra2@gmail.com>
2020-10-25 21:23:35 +0530

    Scroll indicator

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,6 @@ $(window).load(function() {
 });
 */
 
-// AOS Init
-AOS.init({
-	easing: "ease-out-back",
-	duration: 1000,
-});
-
-document.addEventListener("aos:in:super-duper", ({ detail }) => {
-	console.log("in!", detail);
-});
-
 const debounce = (func, delay) => {
 	let debounceTimer;
 	return function () {
@@ -23,7 +13,7 @@ const debounce = (func, delay) => {
 		debounceTimer = setTimeout(() => func.apply(context, args), delay);
 	};
 };
-// debounce( function_name(parameters), 500);
+// example : debounce( function_name(parameters), 500);
 
 /**
  * Text Animation Home Page
@@ -50,45 +40,64 @@ textRotator = function (element) {
 	};
 	timer = setInterval(autoSlide, 3000);
 };
+
 $(function () {
 	textRotator(".change-text span");
 });
 
-
 // Smooth Scrolling
 // Select all links with hashes
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function (event) {
+		// On-page links
+		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$("html, body").animate(
+					{
+						scrollTop: target.offset().top,
+					},
+					1000,
+					function () {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) {
+							// Checking if the target was focused
+							return false;
+						} else {
+							$target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						}
+					}
+				);
+			}
+		}
+	});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+	debounce(scrollIndicator(),10);
+};
+
+function scrollIndicator() {
+	const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  const screenHeiight = screen.height;
+  document.getElementById("scrollBar").style.height = scrolled + "%";
+  if(winScroll >= (height - screenHeiight)){
+    $('.icon-scroll').fadeOut();
+  }else{
+    $('.icon-scroll').fadeIn();
+  }
+}
